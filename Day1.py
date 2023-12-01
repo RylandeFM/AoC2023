a8aad4e0bb799c22b77ccf65f91aae9dff5d3aca
RylandeFM <frankmennink@gmail.com>
2023-12-01 11:34:32 +0100

    Regex approach for part 2

--- Day1.py
+++ Day1.py
@@ -1,9 +1,12 @@
+import re
+
 with open("Input/Day 1.txt", "r") as f: inputString = f.read().splitlines()
 
 def partOne():
     numbers = [[y for y in x if y.isnumeric()] for x in inputString]
     print(sum([int(x[0] + x[-1]) for x in numbers]))
-    
+
+''' Initial attempt
 def partTwo():
     sum, replaces = 0, {"one":'1',"two":'2',"three":'3',"four":'4',"five":'5',"six":'6',"seven":'7',"eight":'8',"nine":'9'}
     for line in inputString:
@@ -24,6 +27,14 @@ def partTwo():
                 break
         sum += int(number[0]+number[1])
     print(sum)
+'''
 
+def partTwo():
+    sum, replaces = 0, {"one":'1',"two":'2',"three":'3',"four":'4',"five":'5',"six":'6',"seven":'7',"eight":'8',"nine":'9'}
+    for line in inputString:
+        numbers = re.findall('(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))',line)
+        sum += int((numbers[0] if numbers[0].isnumeric() else replaces[numbers[0]]) + (numbers[-1] if numbers[-1].isnumeric() else replaces[numbers[-1]]))
+    print(sum)
+        
 partOne()
 partTwo()
