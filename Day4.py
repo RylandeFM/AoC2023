de08f13da8e15eea864ed02d5b6490b61c9cd07c
RylandeFM <frankmennink@gmail.com>
2023-12-04 10:05:42 +0100

    formatting

--- Day4.py
+++ Day4.py
@@ -4,14 +4,14 @@ cards = {}
 
 for i, line in enumerate(inputString):
     numbers = line.split(": ")[1]
-    cards[i + 1] = {"winners":[n for n in numbers.split(" | ")[0].split(" ") if n], "numbers":[n for n in numbers.split(" | ")[1].split(" ") if n]}
+    cards[i + 1] = {"winners": [n for n in numbers.split(" | ")[0].split(" ") if n], "numbers": [n for n in numbers.split(" | ")[1].split(" ") if n]}
     
 def checkScratchers():
-    amounts = {card:1 for card in cards.keys()}
+    amounts = {card: 1 for card in cards.keys()}
     for index, card in cards.items():
         cards[index]["matches"] = len([n for n in card["winners"] if n in card["numbers"]])
         for i in range(index + 1, index + card["matches"] + 1): amounts[i] += amounts[index]
-    print(sum([2 ** (p["matches"]-1) for p in cards.values() if p["matches"] > 0]))
+    print(sum([2 ** (p["matches"] - 1) for p in cards.values() if p["matches"] > 0]))
     print(sum(amounts.values()))
     
 checkScratchers()
