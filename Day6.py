f94580d6ff6bed228959227a1d917a1d5d56e9c0
RylandeFM <frankmennink@gmail.com>
2023-12-07 09:53:56 +0100

    no need to make a range

--- Day6.py
+++ Day6.py
@@ -7,9 +7,9 @@ seconds, goals = re.findall("([0-9]+)",inputString[0]), re.findall("([0-9]+)",in
 def findWays(seconds, goals):
     numberOfWays = 1
     for index, entry in enumerate(seconds):
-        for i in range(goals[index]//entry, entry):
-            if i * (entry-i) > goals[index]: break
-        numberOfWays *= len(range(i, entry-i+1))
+        for i in range(goals[index] // entry, entry):
+            if i * (entry - i) > goals[index]: break
+        numberOfWays *= entry - (i * 2) + 1
     print(numberOfWays)
 
 findWays([int(x) for x in seconds], [int(x) for x in goals])
