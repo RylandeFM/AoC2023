c8cc34792e1b078f4753584a11ac10dd908a480a
RylandeFM <frankmennink@gmail.com>
2023-12-07 11:37:37 +0100

    no need to save the actual card

--- Day7.py
+++ Day7.py
@@ -31,9 +31,9 @@ def camelCards(jokersApply, translationTable):
             case "221": newCard = "3" + newCard # two pair
             case "2111": newCard = "2" + newCard # one pair
             case "11111": newCard = "1" + newCard # high card
-        translatedCards.append([newCard, card[1], int(newCard, 13)])    
+        translatedCards.append([int(newCard, 13), card[1]])    
 
-    print(sum([(i+1) * x[1] for i, x in enumerate(sorted(translatedCards, key=lambda x: x[2]))]))
+    print(sum([(i + 1) * x[1] for i, x in enumerate(sorted(translatedCards, key=lambda x: x[0]))]))
 
 camelCards(False, {'2':'0','3':'1','4':'2','5':'3','6':'4','7':'5','8':'6','9':'7','T':'8','J':'9','Q':'A','K':'B','A':'C'})
 camelCards(True, {'2':'1','3':'2','4':'3','5':'4','6':'5','7':'6','8':'7','9':'8','T':'9','J':'0','Q':'A','K':'B','A':'C'})
