43deb16d13f2426b77b8bd70b06d6eec771d6014
RylandeFM <frankmennink@gmail.com>
2023-12-12 08:20:50 +0100

    Initial Solution

--- Day9.py
+++ Day9.py
@@ -1,1 +1,8 @@
-with open("Input/Day 9.txt", "r") as f: inputString = f.read().splitlines()
+with open("Input/Day 9.txt", "r") as f: inputList = [[int(x) for x in line.split(" ")] for line in f.read().splitlines()]
+
+def getLastElement(r):
+    if set(r) == {0}: return 0
+    return r[-1] + getLastElement([b - a for a, b in zip(r, r[1:])]) 
+    
+print(sum([getLastElement(row) for row in inputList]))
+print(sum([getLastElement(row[::-1]) for row in inputList]))
