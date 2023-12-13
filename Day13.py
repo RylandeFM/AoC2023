7c5d68bc0cd5562db5ce947d1c3ed87f9dcbf6d5
RylandeFM <frankmennink@gmail.com>
2023-12-13 21:24:58 +0100

    With comments so I can understand what the fuck is happening later

--- Day13.py
+++ Day13.py
@@ -4,11 +4,19 @@ patterns = [[line for line in inputString.split("\n")] for inputString in inputS
 
 def getMirrorRow(intList, targetDifferences):
     for i in range(1, len(intList)):
+        #maximum amount of spaces we can look left and right is min(i, len(intList) - i). 
+        #We sum all positions that are different between the left and right half of the window
         if sum(sum(1 for dx, dy in zip(x, y) if dx != dy) for x, y in zip(intList[i-min(i, len(intList) - i):i], 
                                                                           intList[i:i+min(i, len(intList) - i)][::-1])) == targetDifferences: return i
     return 0
 
 def findMirrors(targetDifferences):  
+    ''' so I know what happens when I look at this again later
+    horizontal = sum([getMirrorRow([line for line in pattern], targetDifferences) for pattern in patterns])
+    verticalPatterns = [["".join([pattern[i][j] for i in range(len(pattern))]) for j in range(len(pattern[0]))] for pattern in patterns]
+    vertical = sum([getMirrorRow(pattern, targetDifferences) for pattern in verticalPatterns])
+    print((horizontal * 100) + vertical)
+    '''
     print((sum([getMirrorRow([line for line in pattern], targetDifferences) for pattern in patterns]) * 100) + 
           sum([getMirrorRow(["".join([pattern[i][j] for i in range(len(pattern))]) for j in range(len(pattern[0]))], targetDifferences) for pattern in patterns]))
         
