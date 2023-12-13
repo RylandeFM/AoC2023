6979ba658ebe3d8de761269487612ccda5765d9d
RylandeFM <frankmennink@gmail.com>
2023-12-13 21:00:39 +0100

    Because we like long lines

--- Day13.py
+++ Day13.py
@@ -2,24 +2,15 @@ with open("Input/Day 13.txt", "r") as f: inputStrings = f.read().split("\n\n")
 
 patterns = [[line for line in inputString.split("\n")] for inputString in inputStrings]
 
-def getMirrorRow(intList, target):
+def getMirrorRow(intList, targetDifferences):
     for i in range(1, len(intList)):
-        #see how far to the left and right we can go to check for similars, take the lowest bound as we can't see into the unknown
-        maxWindow = min(i, len(intList) - i)
-        #sum all the different elements in the windows
-        if sum(sum(1 for dx, dy in zip(x, y) if dx != dy) for x, y in zip(intList[i-maxWindow:i], intList[i:i+maxWindow][::-1])) == target: return i
+        if sum(sum(1 for dx, dy in zip(x, y) if dx != dy) for x, y in zip(intList[i-min(i, len(intList) - i):i], 
+                                                                          intList[i:i+min(i, len(intList) - i)][::-1])) == targetDifferences: return i
     return 0
 
-def findMirrors(target):
-    vertical, horizontal = 0,0
-    
-    for pattern in patterns: 
-        horizontalInts = [line for line in pattern]
-        verticalInts = ["".join([pattern[i][j] for i in range(len(pattern))]) for j in range(len(pattern[0]))]
-        horizontal += getMirrorRow(horizontalInts, target)
-        vertical += getMirrorRow(verticalInts, target)
-        
-    print((horizontal * 100) + vertical)
+def findMirrors(targetDifferences):  
+    print((sum([getMirrorRow([line for line in pattern], targetDifferences) for pattern in patterns]) * 100) + 
+          sum([getMirrorRow(["".join([pattern[i][j] for i in range(len(pattern))]) for j in range(len(pattern[0]))], targetDifferences) for pattern in patterns]))
         
 findMirrors(0)
 findMirrors(1)
