e1521590cc5745b8245be6ca0135b637ba78f13b
RylandeFM <frankmennink@gmail.com>
2023-12-14 15:49:25 +0100

    more spacing

--- Day7.py
+++ Day7.py
@@ -18,11 +18,11 @@ def camelCards(jokersApply, translationTable):
             else:
                 for key in occurrences.keys():
                     if key != '0': noJokers.append(occurrences[key])
-                noJokers = sorted(noJokers, reverse=True)
+                noJokers = sorted(noJokers, reverse = True)
                 noJokers[0] += toAdd
                 cardProfile = "".join(map(str, noJokers))
         else:
-            cardProfile = "".join(map(str, sorted(occurrences.values(), reverse=True)))
+            cardProfile = "".join(map(str, sorted(occurrences.values(), reverse = True)))
         match cardProfile:
             case "5": newCard = "7" + newCard # five of a kind
             case "41": newCard = "6" + newCard # four of a kind
@@ -33,7 +33,7 @@ def camelCards(jokersApply, translationTable):
             case "11111": newCard = "1" + newCard # high card
         translatedCards.append([int(newCard, 13), card[1]])    
 
-    print(sum([(i + 1) * x[1] for i, x in enumerate(sorted(translatedCards, key=lambda x: x[0]))]))
+    print(sum([(i + 1) * x[1] for i, x in enumerate(sorted(translatedCards, key = lambda x: x[0]))]))
 
 camelCards(False, {'2': '0', '3': '1', '4': '2', '5': '3', '6': '4', '7': '5', '8': '6', '9': '7', 'T': '8', 'J': '9', 'Q': 'A', 'K': 'B', 'A': 'C'})
 camelCards(True, {'2': '1', '3': '2', '4': '3', '5': '4', '6': '5', '7': '6', '8': '7', '9': '8', 'T': '9', 'J': '0', 'Q': 'A', 'K': 'B', 'A': 'C'})
