0841f7c05154af95d3d2177264335f0a38d2599b
RylandeFM <frankmennink@gmail.com>
2023-12-14 11:50:23 +0100

    More optimized solution

--- Day14.py
+++ Day14.py
@@ -1,53 +1,39 @@
-from time import perf_counter_ns
-
 with open("Input/Day 14.txt", "r") as f: inputString = f.read().splitlines()
 
-rocks, blockers = [], []
+rocks, blockMap = [], [["."] * len(inputString) for _ in range(len(inputString[0]))]
 
 for y, line in enumerate(inputString):
     for x, c in enumerate(line):
         if c == "O": rocks.append((x, y))
-        if c == "#": blockers.append((x, y))
-        
-def tumble(rocks, direction):
-    newRocks, newBlockers = [], blockers[:]
+        if c == "#": blockMap[y][x] = "#"
+
+def tumbleMapped(rocks, direction):
+    newRocks, newBlockMap = [], [row[:] for row in blockMap]
     if direction[0] != 0:
-        rocks = sorted(rocks, key=lambda x: (x[0] if direction[0] == -1 else -x[0], x[1]))
+        rocks = sorted(rocks, key=lambda x: (x[0] if direction[0] == -1 else -x[0]))
     else:
-        rocks = sorted(rocks, key=lambda x: (x[0], x[1] if direction[1] == -1 else -x[1]))
+        rocks = sorted(rocks, key=lambda x: (x[1] if direction[1] == -1 else -x[1]))
+        
     for rock in rocks:
-        while (rock[0] > 0 if direction[0] == -1 else rock[0] < len(inputString[0])-1 if direction[0] != 0 else rock[1] > 0 if direction[1] == -1 else rock[1] < len(inputString)-1) and (rock[0] + direction[0], rock[1] + direction[1]) not in newBlockers:
+        while (rock[0] > 0 if direction[0] == -1 else rock[0] < len(inputString[0])-1 if direction[0] != 0 else rock[1] > 0 if direction[1] == -1 else rock[1] < len(inputString)-1) and newBlockMap[rock[1] + direction[1]][rock[0] + direction[0]] != "#":
             rock = (rock[0] + direction[0], rock[1] + direction[1])
         newRocks.append(rock)
-        newBlockers.append(rock)
-    '''
-    for y in range(len(inputString)):
-        line = ""
-        for x in range(len(inputString[0])):
-            if (x, y) in newRocks: line+="O"
-            elif (x, y) in blockers: line+="#"
-            else: line += "."
-        print(line)
-    print("\n")
-    '''
+        newBlockMap[rock[1]][rock[0]] = "#"
+    
     return newRocks
     
 def partOne():
-    print(sum([len(inputString) - r[1] for r in tumble(rocks, (0, -1))]))
+    print(sum([len(inputString) - r[1] for r in tumbleMapped(rocks, (0, -1))]))
     
 def partTwo(r):
     rockLayouts, rocks = [], r[:]
     while rocks not in rockLayouts:
         rockLayouts.append(rocks)
-        rocks = tumble(tumble(tumble(tumble(rocks,(0,-1)),(-1,0)),(0,1)),(1,0))
+        rocks = tumbleMapped(tumbleMapped(tumbleMapped(tumbleMapped(rocks,(0,-1)),(-1,0)),(0,1)),(1,0))
 
     startOfCycle = rockLayouts.index(rocks)
     targetIndex = ((1000000000 - startOfCycle) % (len(rockLayouts) - startOfCycle)) + startOfCycle
     print(sum([len(inputString) - r[1] for r in rockLayouts[targetIndex]]))
     
-start = perf_counter_ns()
 partOne()
-print((perf_counter_ns()-start)/1000000)
-start = perf_counter_ns()
 partTwo(rocks)
-print((perf_counter_ns()-start)/1000000)
