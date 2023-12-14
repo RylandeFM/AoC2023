ab001ddabb30814e20d9a61eba185cf28d4c06a2
RylandeFM <frankmennink@gmail.com>
2023-12-14 16:03:01 +0100

    One method solution

--- Day8.py
+++ Day8.py
@@ -5,23 +5,16 @@ with open("Input/Day 8.txt", "r") as f: inputString = f.read().splitlines()
 instructions, roadMap = inputString[0], {}
 for line in inputString[2:]:
     roadMap[line.split(" = ")[0]] = {"L": line.split("(")[1].split(", ")[0], "R": line.split("(")[1].split(", ")[1].split(")")[0]}
-    
-def partOne():
-    currentNode, step = "AAA", 0
-    while currentNode != "ZZZ":
-        currentNode = roadMap[currentNode][instructions[step % len(instructions)]]
-        step += 1
-    print(step)
 
-def partTwo():
-    startNodes, stepsToEnd = [x for x in roadMap.keys() if x[2] == "A"], []
+def getCycles(startEnding, endEnding):
+    startNodes, stepsToEnd = [x for x in roadMap.keys() if x.endswith(startEnding)], []
     for startNode in startNodes:
         currentNode, step = startNode, 0
-        while currentNode[2] != "Z":
+        while not currentNode.endswith(endEnding):
             currentNode = roadMap[currentNode][instructions[step % len(instructions)]]
             step += 1
         stepsToEnd.append(step)
     print(lcm(*stepsToEnd))
-
-partOne()
-partTwo()
+    
+getCycles("AAA", "ZZZ")
+getCycles("A", "Z")
