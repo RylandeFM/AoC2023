e9a317aa9f8e688fbab5941203db3ca19aaf2f8c
RylandeFM <frankmennink@gmail.com>
2023-12-16 16:01:08 +0100

    Optimization

--- Day16.py
+++ Day16.py
@@ -1,13 +1,18 @@
 with open("Input/Day 16.txt", "r") as f: inputString = f.read().splitlines()
 
-floorMap, directions = [[c for c in line]for line in inputString], {"R": (1, 0), "L": (-1, 0), "U": (0, -1), "D": (0, 1)}
+floorMap, directions = [[c for c in line]for line in inputString], {"R": (1, 0), "L": (-1, 0), "U": (0, -1), "D": (0, 1)}, 
+opposite = {"R": "L", "L": "R", "U":"D", "D": "U"}
         
 def energizeFloor(start):
-    energized, toVisit, visited = set(), [], set()
+    energized, toVisit, visited, exits = set(), [], set(), set()
     toVisit.append(start)
     while toVisit:
         current = toVisit.pop()
-        if current[0] not in range(0, len(floorMap[0])) or current[1] not in range(0, len(floorMap)) or current in visited: continue
+        if current in visited: continue
+        if current[0] not in range(0, len(floorMap[0])) or current[1] not in range(0, len(floorMap)):
+            toApply = directions[opposite[current[2]]]
+            exits.add((current[0] + toApply[0], current[1] + toApply[1], opposite[current[2]]))
+            continue
         energized.add((current[0], current[1]))
         visited.add(current)
         newX, newY = current[0] + directions[current[2]][0], current[1] + directions[current[2]][1]
@@ -47,15 +52,23 @@ def energizeFloor(start):
                     toVisit.append((current[0] + 1, current[1], "R"))
                 else:
                     toVisit.append((newX, newY, current[2]))
-    
-    return len(energized)
+    return len(energized), exits
 
 def findMaxEnergy():
-    rights = max([energizeFloor((0, y, "R")) for y in range(len(inputString))])
-    lefts = max([energizeFloor((len(inputString[0]) - 1, y, "L")) for y in range(len(inputString))])
-    downs = max([energizeFloor((x, 0, "D")) for x in range(len(inputString[0]))])
-    ups = max([energizeFloor((x, len(inputString) - 1, "U")) for x in range(len(inputString[0]))])
-    return max(rights, lefts, downs, ups)
+    toCheck, energizes = [], []
+    for y in range(len(inputString)): 
+        toCheck.append((0, y, "R"))
+        toCheck.append((len(inputString[0]) - 1, y, "L"))
+    for x in range(len(inputString[0])):
+        toCheck.append((x, 0, "D"))
+        toCheck.append((x, len(inputString) - 1, "U"))
+    
+    while toCheck:
+        energy, toRemove = energizeFloor(toCheck.pop())
+        energizes.append(energy)
+        for r in toRemove:
+            if r in toCheck: toCheck.remove(r)
+    print(max(energizes))
 
-print(energizeFloor((0, 0, "R")))
-print(findMaxEnergy())
+print(energizeFloor((0, 0, "R"))[0])
+findMaxEnergy()
